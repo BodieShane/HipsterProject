2c9cefef81b13b64e14cfb2e1e343b339b96963f
Bodie Shane <bodieshane68@gmail.com>
2015-11-19 08:23:56 -0700

    Added all the books. Set everything in them.

--- src/hipster/model/Hipster.java
+++ src/hipster/model/Hipster.java
@@ -4,13 +4,42 @@ public class Hipster
 {
 	private String name;
 	private String [] hipsterPhrases;
+	private Book[]hipsterBooks;
+	
+
 	
 	public Hipster()
 	{
 		this.name = "";
 		this.hipsterPhrases = new String[4];
 		setupArray();
-		//setupBoooks();
+	
+		setupBoooks();
+		
+	}
+	private  void setupBoooks()
+	{
+		Book firstBook, secondBook, thirdBook;
+		firstBook= new Book();
+		firstBook.setAuthor("Bodie Shane");
+		firstBook.setTitle("Code Age");
+		firstBook.setSubject("ScienceFiction");
+		firstBook.setPageCount(301);
+		firstBook.setPrice(300.48);
+		
+		secondBook = new Book();
+		secondBook.setAuthor("not Bodie Shane");
+		secondBook.setTitle("This will never be as good as Code Age");
+		secondBook.setSubject("HistoryNonFiction");
+		secondBook.setPageCount(1);
+		secondBook.setPrice(-300.47);
+		
+		thirdBook = new Book(5, "Bodie Shane", "Jump Ball", "Fiction", 999.99);
+		
+		hipsterBooks[0] = firstBook;
+		hipsterBooks[1] = secondBook;
+		hipsterBooks[2] = thirdBook;
+		
 		
 	}
 	public void setupArray()
@@ -40,5 +69,11 @@ public class Hipster
 	public void setHipsterPhrases(String[] hipsterPhrases)
 	{
 		this.hipsterPhrases = hipsterPhrases;
+		public Book[] getHipsterBooks()
+	{
+		return hipsterBooks;
 	}
+	public void setHipsterBooks(Book[] hipsterBooks)
+	{
+		this.hipsterBooks = hipsterBooks;}
 }
